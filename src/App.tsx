2f665d687ed802132d03d7048af8ecfa2541ddc9
evenilsonlp <evenilsonlp@gmail.com>
2022-03-22 09:20:47 -0300

    feat: add hook to handle responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { BrowserRouter } from 'react-router-dom';
 import Header from './components/Header';
+import BreakpointDeviceContextProvider from './contexts/BreakpointDeviceContext';
 import RoutesApp from './routes';
 import './styles/global.scss';
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <RoutesApp />
+      <BreakpointDeviceContextProvider>
+        <Header />
+        <RoutesApp />
+      </BreakpointDeviceContextProvider>
     </BrowserRouter>
   );
 }
