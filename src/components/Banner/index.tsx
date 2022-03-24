3f77a6cdfaa38289e1134980c6ee3ed562499ea1
evenilsonlp <evenilsonlp@gmail.com>
2022-03-24 20:16:16 -0300

    fix: header changes

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,19 +1,20 @@
 
 
-import { CSSProperties, ReactNode } from 'react';
+import { ReactNode } from 'react';
 import './styles.scss'
 
 type BannerProps = {
   title: string | ReactNode;
   children?: ReactNode
   styles?: React.CSSProperties
+  styleTitle?: React.CSSProperties
 }
 
-function Banner({title, children, styles={}}: BannerProps) {
+function Banner({title, children, styles={}, styleTitle = {}}: BannerProps) {
   return (
-    <section>
+    <section className="banner">
       <div className="container banner-content" style={styles}>
-        <h1>
+        <h1 style={styleTitle}>
           {title}
         </h1>
         {children}
